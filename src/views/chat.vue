66e52434b9aee59fbbb83d56e00ffe5988441716
WuWenkang <wwk981124@gmail.com>
2023-04-04 18:18:48 +0800

    fix(router): 路由地址修改
    1. 路由地址修改适配GitHub Pages

--- src/views/chat.vue
+++ src/views/chat.vue
@@ -2,7 +2,7 @@
   <q-drawer show-if-above side="left" elevated mini>
     <q-list class="fixed-center full-width">
       <q-item clickable v-ripple :active="menu === 'message'" class="text-secondary" @click="menu='message'"
-              to="/chat/message">
+              to="/mashed_potatoes_ui/chat/message">
         <q-item-section avatar>
           <q-btn round icon="o_mark_unread_chat_alt" :class="menu === 'message'? 'bg-secondary text-white':''">
             <q-badge floating rounded color="red">2</q-badge>
@@ -11,7 +11,7 @@
       </q-item>
 
       <q-item clickable v-ripple :active="menu === 'friend'" class="text-secondary" @click="menu='friend'"
-              to="/chat/friend">
+              to="/mashed_potatoes_ui/chat/friend">
         <q-item-section avatar>
           <q-btn round icon="o_people_alt" :class="menu === 'friend'? 'bg-secondary text-white':''">
           </q-btn>
