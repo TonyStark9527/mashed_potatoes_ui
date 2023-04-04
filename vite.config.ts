98b7758757c35c089316c739199a19d9788a2b74
WuWenkang <wwk981124@gmail.com>
2023-04-04 14:12:39 +0800

    fix(vite.config.ts): 修复baseUrl
    1. 修复baseUrl

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
+    base: '/mashed_potatoes_ui/',
     plugins: [
         vue({
             template: {transformAssetUrls}
