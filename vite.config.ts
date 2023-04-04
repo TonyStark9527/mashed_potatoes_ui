4b03eed4cfbac97bf69c39272e208c328d058d5c
WuWenkang <wwk981124@gmail.com>
2023-04-04 16:06:46 +0800

    fix(vite.config.ts): 修复部分静态资源服务器无法正常返回的问题
    1. 对文件名进行重写

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import {quasar, transformAssetUrls} from "@quasar/vite-plugin"
 import {resolve} from 'path'
 
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
+
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/mashed_potatoes_ui/',
@@ -17,6 +20,19 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': resolve(__dirname, './src')
+        }
+    },
+    build:{
+        rollupOptions: {
+            output: {
+                sanitizeFileName(name: string) {
+                    const match = DRIVE_LETTER_REGEX.exec(name)
+                    const driveLetter = match ? match[0] : ''
+                    return (
+                        driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+                    )
+                }
+            }
         }
     },
     server: {
