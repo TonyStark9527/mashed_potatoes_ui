b75da1a223a88fd09e9d85e48484c831d40a7581
WuWenkang <wwk981124@gmail.com>
2023-04-11 16:34:01 +0800

    feat(App): 新增用户登录相关逻辑
    1. 新增用户登录相关逻辑
    2. 优化文本之间的距离

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,15 @@
           Mashed_potatoes
         </q-toolbar-title>
         <q-tabs v-model="menu" inline-label class="bg-secondary">
-          <q-route-tab name="chat" icon="chat" label="聊天" to="/mashed_potatoes_ui/chat">
+          <q-route-tab name="chat" icon="chat" label="聊 天" to="/mashed_potatoes_ui/chat">
             <q-badge floating rounded color="red">2</q-badge>
           </q-route-tab>
-          <q-route-tab name="folder" icon="folder" label="文件" to="/mashed_potatoes_ui/folder"/>
-          <q-tab name="notice" icon="notifications_active" label="通知">
+          <q-route-tab name="folder" icon="folder" label="文 件" to="/mashed_potatoes_ui/folder"/>
+          <q-tab name="notice" icon="notifications_active" label="通 知">
             <q-badge floating rounded color="red">2</q-badge>
           </q-tab>
-          <q-btn-dropdown auto-close stretch flat>
+          <q-btn v-if="!login.isLogin" class="no-shadow" color="secondary" icon="person" label="登 录" stretch unelevated @click="login.loginPanel = true"/>
+          <q-btn-dropdown v-if="login.isLogin" auto-close stretch flat>
             <template v-slot:label>
               <q-avatar>
                 <q-img src="https://cdn.quasar.dev/img/boy-avatar.png"/>
@@ -51,6 +52,25 @@
       <router-view/>
     </q-page-container>
   </q-layout>
+  <q-dialog v-model="login.loginPanel" persistent>
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">登 录</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <q-form class="q-gutter-md">
+          <q-input standout="bg-teal text-white" label="用户名" v-model="login.username"/>
+          <q-input standout="bg-teal text-white" label="密码" v-model="login.password"/>
+        </q-form>
+      </q-card-section>
+
+      <q-card-actions align="right" class="text-primary">
+        <q-btn unelevated color="secondary" label="取 消" v-close-popup />
+        <q-btn unelevated color="secondary" label="登 录" :loading="login.loginLoading" @click="loginIn" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 
 <script setup lang="ts">
@@ -58,7 +78,6 @@
 import {useQuasar} from 'quasar'
 import {ref, watch} from 'vue'
 import MessageTip from './utils/messageTip'
-import {useRoute, useRouter} from 'vue-router'
 import api from './api/axios'
 
 // 主题样式
@@ -66,22 +85,38 @@ const theme = ref(true)
 
 const $q = useQuasar()
 
-const route = useRoute()
+const login = ref({
+  loginPanel: false, // 是否弹出登录面板
+  loginLoading: false, // 调用登录接口时，登录按钮的loading状态
+  username: 'wwk', // 用户名
+  password: '123456', // 用户密码
+  isLogin: false // 用户菜单显示控制，true为登录菜单，false为未登录菜单
+})
 
-const router = useRouter()
-
-console.log(route)
-console.log(router)
+function loginIn() {
+  login.value.loginLoading = true
+  // api.post('/user/login', {username: login.value.username, password: login.value.password}).then(res => {
+  //   // todo 处理返回参数
+  // })
+  // todo 实现用户登录接口的调用，以及用户token的存储与使用
+  // 这里是模拟用户登录接口调用耗时2秒
+  window.setTimeout(() => {
+    // 关闭登录按钮loading状态
+    login.value.loginLoading = false
+    // 关闭登录弹框
+    login.value.loginPanel = false
+    // 用户菜单切换为已登录菜单
+    login.value.isLogin = true
+    // 提示用户登录成功
+    MessageTip.success('登录成功！')
+  }, 2000)
+}
 
 const menu = ref('chat')
 
 watch(theme, (newShow, oldShow) => {
   $q.dark.set(!newShow)
   MessageTip.success('更换主题成功！')
-})
-
-api.post('/user/login', {username: 'wwk', password: '123456'}).then(res => {
-  // todo 处理返回参数
 })
 
 </script>
