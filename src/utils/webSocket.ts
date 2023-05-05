9c0695fdfd30c350ff50a3fb4dc522e930a6d289
WuWenkang <wwk981124@gmail.com>
2023-05-05 17:18:22 +0800

    feat(webSocket): 新增webSocket工具
    1. 新增webSocket工具

---
+++ src/utils/webSocket.ts
@@ -0,0 +1,33 @@
+import NotifyTip from "@/utils/notify"
+
+module webSocketUtil {
+    export function create(url: string, messageCallBack: Function) {
+        if ('WebSocket' in window) {
+            let webSocket = new WebSocket(url)
+            webSocket.onerror = function (event) {
+                NotifyTip.error('websocket连接失败！')
+            }
+            webSocket.onopen = function (event) {
+                NotifyTip.success('websocket连接成功！')
+            }
+            webSocket.onmessage = function (messageEvent: MessageEvent) {
+                messageCallBack(messageEvent)
+            }
+            webSocket.onclose = function (closeEvent) {
+                NotifyTip.success('websocket关闭成功！')
+            }
+            window.onbeforeunload = function () {
+                closeWebSocket(webSocket)
+            }
+            return webSocket
+        } else {
+            NotifyTip.warn('该浏览器不支持WebSocket!')
+        }
+    }
+
+    function closeWebSocket(webSocket: WebSocket) {
+        webSocket.close()
+    }
+}
+
+export default webSocketUtil
