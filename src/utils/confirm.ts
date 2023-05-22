0752bd043c48748ab37a3ffac6c152a14aeace55
WuWenkang <wwk981124@gmail.com>
2023-05-22 16:04:18 +0800

    feat(confirm): 新增confirm模块
    1. 新增确认通用模块

---
+++ src/utils/confirm.ts
@@ -0,0 +1,33 @@
+import {Dialog} from "quasar"
+
+module ConfirmTip {
+    export function confirm(message: string,
+                            ok: Function,
+                            cancel: Function | undefined = undefined,
+                            dismiss: Function | undefined = undefined,
+                            title: string = '请确认') {
+        Dialog.create({
+            title: title,
+            message: message,
+            ok: {
+                color: 'secondary'
+            },
+            cancel: {
+                color: 'negative'
+            },
+            persistent: true
+        }).onOk(() => {
+            ok()
+        }).onCancel(() => {
+            if (cancel) {
+                cancel()
+            }
+        }).onDismiss(() => {
+            if (dismiss) {
+                dismiss()
+            }
+        })
+    }
+}
+
+export default ConfirmTip
