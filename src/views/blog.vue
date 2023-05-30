2f435c8550ee7d085d69cc8e4203b614d6b0bef3
WuWenkang <wwk981124@gmail.com>
2023-05-30 16:52:02 +0800

    chore(vditor): 引入vditor
    1. 引入vditor
    2. vditor样例

---
+++ src/views/blog.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="vditor"></div>
+</template>
+
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import Vditor from "vditor"
+import 'vditor/dist/index.css'
+
+const vditor = ref<Vditor | null>(null)
+
+onMounted(() => {
+  vditor.value = new Vditor('vditor', {
+    height: '100%',
+    theme: 'dark',
+    mode: 'sv',
+    toolbar: ['emoji'],
+    after: () => {
+      vditor.value!.setValue('Vue Composition API + Vditor + TypeScript Minimal Example')
+    }
+  })
+})
+</script>
